75096997d4ec68709b5b33c24c20613bbf224885
leogtzr <leogutierrezramirez@gmail.com>
2020-10-26 20:12:01 -0700

    change README and change script

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # processkiller
 Kills linux processes based on a name pattern
+
+## Usage examples
+
+./processkiller.sh sleep
+./processkiller.sh -i Firefox
+
--- processkiller.sh
+++ processkiller.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# leogtz
+
 set -o pipefail
 set -o errexit
 set -o nounset
 
-readonly work_dir=$(dirname "$(readlink --canonicalize-existing "${0}")")
+# readonly work_dir=$(dirname "$(readlink --canonicalize-existing "${0}")")
 
 show_help() {
 cat <<HELP_TEXT
@@ -16,6 +18,48 @@ $(echo -e "\033[1mOPTIONS\033[0m")
     -h, --help Output a usage message and exit.
 
 HELP_TEXT
+}
+
+process_list() {
+    local -r process_name="${1}"
+    local -r ignore="${2}"
+
+    if [[ -z "${process_name}" ]]; then
+        return 1
+    fi
+    if [[ -z "${ignore}" ]]; then
+        return 1
+    fi
+    if ((ignore)); then
+        ps -ef | grep --ignore-case "${process_name}"
+    else
+        ps -ef | grep "${process_name}"
+    fi
+}
+
+kill_p() {
+    local -r pid="${1}"
+    if [[ -z "${pid}" ]]; then
+        return 1
+    fi
+
+    kill -kill "${pid}"
+}
+
+kill_process() {
+    local -r process_name="${1}"
+    local -r ignore="${2}"
+    local process
+    local pid
+
+    while read process; do
+        pid=$(awk '{print $2}' <<< "${process}")
+        if ! kill_p "${pid}"; then
+            printf "Could not kill '%s' with %d pid\n" "${process_name}" "${pid}"
+        else
+            printf "%s [%d] killed\n" "${process_name}" "${pid}"
+        fi
+    done < <(process_list "${process_name}" "${ignore}" | grep --invert-match "gre[p]" | grep --invert-match "${0}")
 }
 
 readonly opts=$(getopt -o ih --long ignore-case,help -- "${@}" 2> /dev/null)
@@ -49,7 +93,8 @@ while true; do
     esac
 done
 
-echo "Ignore: ${ignore_case_flag}"
-echo "Args -> [${@}]"
+for process in "${@}"; do
+    kill_process "${process}" "${ignore_case_flag}"
+done
 
 exit 0
